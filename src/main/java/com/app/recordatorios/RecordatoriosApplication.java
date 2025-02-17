41c212ff9b76ee5f6915da2d1f1bf76c3aed958a
DavidUmiri <davidumiri@gmail.com>
2025-02-17 19:39:57 +0100

    Amazon Elastic BeanStalk

--- src/main/java/com/app/recordatorios/RecordatoriosApplication.java
+++ src/main/java/com/app/recordatorios/RecordatoriosApplication.java
@@ -2,12 +2,22 @@ package com.app.recordatorios;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class RecordatoriosApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RecordatoriosApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RecordatoriosApplication.class, args);
+    }
 
+    @Configuration
+    public class WebConfig implements WebMvcConfigurer {
+        @Override
+        public void addViewControllers(ViewControllerRegistry registry) {
+            registry.addRedirectViewController("/", "/tarjetas");
+        }
+    }
 }
